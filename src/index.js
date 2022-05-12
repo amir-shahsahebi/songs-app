6c2b3b0f0d627b224dce615877338843587472ec
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-12 10:08:31 +0430

    reducer updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
-import thunk from "redux-thunk";
 // in redux we can not use async data in our actions and we need a middleware to handle it, so we use redux-thunk as a middleware to solve async data,, => so we can pass a function throw our action creator and use async function in redux
 const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
