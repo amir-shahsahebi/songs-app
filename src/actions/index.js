faed1fc8d6a582d5c557666ecff36a635a5a3140
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-12 13:12:06 +0430

    repetitive requests sovled via define ext func

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
+import _ from "lodash"; // this is a lodash library
+
+//   // simple form of function (action creator)
+// export const fetchUser = function (id) {
+//   return async function (dispatch) {
+//     const response = await jsonPlaceHolder.get(`users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   };
+// };
+
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 // when we use a middleware like redux-thunk in our redux, we can pass a function to action creators and it takes two argument named dispatch amd getState and do not need to return an object inside of that, so we call dispatch and make an object inside of that
 export const fetchPosts = () => async (dispatch) => {
@@ -7,7 +17,31 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonPlaceHolder.get(`users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// };
+
+// // we use memoize in external function to avoid from repetitive request for repetitive id but issue still exist so try to change inner function
+// export const fetchUser = _.memoize(function (id) {
+//   // memoize is a function from lodash library that return a function and of the argument (id in this func) is repetitive, it wont work and its benefit of that
+//   return async function (dispatch) {
+//     const response = await jsonPlaceHolder.get(`users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   };
+// });
+
+// // this also have issue (repetitive network requests)
+// export const fetchUser = function (id) {
+//   return _.memoize(async function (dispatch) {
+//     const response = await jsonPlaceHolder.get(`users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   });
+// };
+
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get(`users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
